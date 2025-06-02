f5b41cabf8413e48a39996a383dc8a1f0222d507
Otávio Lorenzo <otaviolorenzo8@gmail.com>
2025-06-02 15:51:47 -0300

    Visualizando todas as frases

--- app/src/main/java/com/otaviolorenzo/aulafrasesdodia/MainActivity.java
+++ app/src/main/java/com/otaviolorenzo/aulafrasesdodia/MainActivity.java
@@ -2,6 +2,7 @@ package com.otaviolorenzo.aulafrasesdodia;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +13,18 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     //Atributo
-
+    String[] frases = {
+            "Riqueza não começa na carteira, começa na mente",
+            "Quem planta disciplina, colhe liberdade financeira",
+            "O dinheiro corre para quem resolve problemas, não para quem foge deles",
+            "A riqueza é consequência de valor entregue ao mundo",
+            "Sonhar é grátis, mas enriquecer custa foco, ação e consistência",
+            "Se você não trabalhar pelos seus sonhos, trabalhará pelos sonhos de quem trabalhou pelos deles",
+            "O dinheiro não some, ele apenas muda de mãos — que seja para as suas",
+            "A diferença entre quem enriquece e quem não enriquece está nas decisões diárias, não em sorte",
+            "Invista mais em conhecimento do que em ostentação, e a riqueza será inevitável",
+            "O dinheiro é um reflexo do valor que você gera para o mundo"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +44,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void exibirTodas (View view){
 
+        TextView texto = findViewById(R.id.textResultado);
+
+        String textoResultado = "";
+        for (String frase : frases ) {
+            textoResultado = textoResultado + frase + "\n";
+        }
+        texto.setText( textoResultado);
     }
 }
